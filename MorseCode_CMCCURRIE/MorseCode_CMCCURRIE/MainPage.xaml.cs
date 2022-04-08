aaec733a21585930a9d9593b8fa1570bacdf0792
Connor <c.mccurrie@gmail.com>
2022-04-08 15:51:46 -0500

    Initial commit

---
+++ MorseCode_CMCCURRIE/MorseCode_CMCCURRIE/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MorseCode_CMCCURRIE
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private string dotsDashes = "";
+        private string message = "";
+        void DotMethod(object sender, System.EventArgs e)
+        {
+            dotsDashes += ".";
+            dotsndashes.Text = dotsDashes;
+        }
+
+        void DashMethod(object sender, System.EventArgs e)
+        {
+            dotsDashes += "-";
+            dotsndashes.Text = dotsDashes;
+        }
+
+        void GapMethod(object sender, System.EventArgs e)
+        {
+            message += Morse.MorseCoder(dotsDashes);
+            english.Text = message;
+            dotsDashes = ""; 
+        }
+    }
+}
